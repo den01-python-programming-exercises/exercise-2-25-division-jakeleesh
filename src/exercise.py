c595c45e57b1a84dfb536493a1a5b0a9bc416b35
Jake Lee <jake.lee@fdmgroup.com>
2021-09-22 22:37:02 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    numerator = int(input())
+    denominator = int(input())
+    division(numerator, denominator)
+
+def division(numerator, denominator):
+    print(numerator / denominator)
 
 if __name__ == '__main__':
     main()
